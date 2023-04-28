593b0dbba8f44b79014a4396a3a1611ea87b632e
weikangqi <3271045267@qq.com>
2023-04-28 16:01:02 +0800

    =change web,add mysql

--- gradio/web.py
+++ gradio/web.py
@@ -16,13 +16,14 @@ def to_black(image):
     
     return output
 
-def f3():
-    os.system("python demo.py")
+
 classification = {0:"正坐",1:"右歪头",2:"左歪头",3:"右抬肩",4:"左抬肩",5:"低头",6:"仰头"}
 
+
+
 mytab4 = gr.Blocks()
-def f4():
-    f =open("data.txt")
+def f4(t):
+    f =open("data.txt",'r')
     x = int(f.readline())
     simple = pd.DataFrame(
         {
@@ -30,17 +31,28 @@ def f4():
             "百分比": [28, 55, 43, 91, 81, 53,  x],
         }
     )
+    f.close()
+    print(t)
     return simple
-
-
-
-
 with mytab4 as m4:
-    gr.Markdown("hello")
+    gr.Markdown("输入要查询的日期 \n格式 : yyyy-mm-dd")
+    te = gr.Textbox()
     btn = gr.Button(value="show")
+     
     station = gr.BarPlot(x="坐姿", y="百分比",vertical  =False,title="坐姿统计分析",y_title="百分比%")
-    btn.click(f4,inputs=None,outputs=station)
- 
+    btn.click(f4,inputs=te,outputs=station)
+
+mytab3 = gr.Blocks()
+def f3():
+    os.system("python demo.py")
+with mytab3 as m3:
+    gr.Markdown("打开前置摄像头")
+   
+    btn = gr.Button(value="打开")
+    
+    btn.click(f3,inputs=None,outputs=station)
+
+    
 function1 = gr.Interface(
                   video_identity,
                 inputs="image", outputs="image",
@@ -53,27 +65,30 @@ function2 = gr.Interface(fn = to_black,
                     inputs="image", outputs="image",
                     cache_examples=False)
 
-function3 = gr.Interface(video_identity, 
-                    gr.Button(), 
-                    "playable_video", 
-                    title = "摄像头",
-                    cache_examples=False)
+
 
 function5 = gr.Interface(video_identity, 
                     gr.Button(), 
                     "playable_video", 
                     title = "控制面板",
                     cache_examples=False)
+def calculator( operation):
+        print(operation)
+        return operation + ["eee"]
 
-function4 = gr.Interface(video_identity, 
-                    gr.Button(), 
-                    "playable_video", 
-                    title = "分析",
-                    cache_examples=False)
+m5 = gr.Interface(
+    calculator,
+    gr.CheckboxGroup(["微信", "邮件", "短信"], label="选择发送工具"),
+    outputs="text",
+    live=True,
+) 
+    
+    
 
-demo2 = gr.TabbedInterface([function1, function2,function3,m4,function5],["视频","图片","摄像头","数据分析","控制面板"],title="合工大 韦康琦 毕业设计 基于计算机视觉的坐姿检测系统")
+
+demo2 = gr.TabbedInterface([function1, function2,m3,m4,m5],["视频","图片","摄像头","数据分析","控制面板"],title="合工大 韦康琦 毕业设计 基于计算机视觉的坐姿检测系统")
 
 if __name__ == "__main__":
-    f3()
-    # demo2.launch()
+    #f3()
+    demo2.launch()
     
